245d522346c87a3e7eaff40873f6ca08b3a0a13e
Brynn mcGarry-Watson <brynnmcgarry-watson@Brynns-MacBook-Pro.local>
2022-12-16 01:23:37 +1100

    adding resume starter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 import vue from "@astrojs/vue"
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), mdx()],
   vite: {
     ssr: {
       noExternal: ["modern-normalize"],
