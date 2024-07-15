c37fc3ddd8fb56850809a8befa8163de7ccbf547
Brynn Briedis <brynn@pragma.com.au>
2024-07-16 01:14:24 +1000

    fixing posts

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,7 @@
 const postcssJitProps = require("postcss-jit-props")
 const OpenProps = require("open-props")
-const postcssCustomMedia = require("postcss-custom-media")
 const postcssNested = require("postcss-nested")
+const postcssCustomMedia = require("postcss-custom-media")
 module.exports = {
-  plugins: [
-    postcssJitProps(OpenProps),
-    postcssCustomMedia({
-      importFrom: "./node_modules/open-props/media.min.css",
-    }),
-    postcssNested,
-  ],
+  plugins: [postcssJitProps(OpenProps), postcssNested, postcssCustomMedia],
 }
