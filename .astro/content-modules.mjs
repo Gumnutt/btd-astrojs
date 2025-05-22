df827fe0b93918352dd038d90dc9eedb0934e258
Brynn Briedis <brynn@pragma.com.au>
2025-05-23 01:41:32 +1000

    update

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/posts/css-gradients-on-borders.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fcss-gradients-on-borders.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/next-level-css-variables-with-at-property.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fnext-level-css-variables-with-at-property.mdx&astroContentModuleFlag=true")],
+["src/content/posts/css-gradients-on-borders.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fcss-gradients-on-borders.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/CSS-Magic.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2FCSS-Magic.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/Ditch-the-setup-struggle.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2FDitch-the-setup-struggle.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/data-pipelines.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fdata-pipelines.mdx&astroContentModuleFlag=true")],
