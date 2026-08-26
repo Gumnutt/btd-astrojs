9aa2e705154289d6afd3b8e0440d2aa1defacd4e
Brynn Briedis <brynn@pragma.com.au>
2026-08-26 14:31:23 +1000

    rebuild form a clean slate

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-const postcssJitProps = require("postcss-jit-props")
-const OpenProps = require("open-props")
-const postcssNested = require("postcss-nested")
-const postcssCustomMedia = require("postcss-custom-media")
-module.exports = {
-  plugins: [postcssJitProps(OpenProps), postcssNested, postcssCustomMedia],
-}
